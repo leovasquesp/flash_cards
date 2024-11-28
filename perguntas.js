02475e4c2542157881a0c3b7cf37fad138f2fa29
leovasquesp <168106551+leovasquesp@users.noreply.github.com>
2024-11-28 11:15:04 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -28,6 +28,80 @@ criaCartao(
    'Taylor Swift'
 )
 
+criaCartao(
+    'Música',
+    'Qual a música mais tocada no mundo?',
+    'Blinding light (The Weekend)'
+)
 
+criaCartao(
+    'Música',
+    'Qual a premiação mais importante da indústria musical?',
+    'Grammy Awarls'
+)
 
-    
+criaCartao(
+    'Animais',
+    'Qual é o maior animal terrestre?',
+    'Elefante africano'
+)
+
+criaCartao(
+    'Animais',
+    'Quantas formigas existem no planeta?,
+    'Cerca de 20 quadrilhões'
+)
+
+criaCartao(
+    'Animais',
+    'Existem mais animais terrestre ou aquáticos?',
+    'Terrestres'
+)
+
+criaCartao(
+    'Geografia',
+    'Em quais os países se localizam as cordilheiras dos Andes?',
+    'Chile, Argentina, Bolívia, Peru, Colômbia e Equador'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos países existem na África?',
+    '54 países'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o menor país do mundo?',
+    'Vaticano'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a revolução francesa?',
+    '1799'
+)
+
+criaCartao(
+    'História',
+    'Em que ano iniciou o holocausto?',
+    '1933'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual o time com mais títulos internacionais no mundo?',
+    'Real Madrid'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantos times tem na NBA?',
+    '30'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual nadador mais vitorioso da história?',
+    'Michael Phelps'
+)
